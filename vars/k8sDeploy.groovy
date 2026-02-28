5e63329512cdbcccda74ea181619e83a3398c5e4
Leonardo Diaz <leonardoleandrodiaz96@gmail.com>
2026-02-28 03:23:57 -0300

    fixed commit

--- vars/k8sDeploy.groovy
+++ vars/k8sDeploy.groovy
@@ -1,10 +1,23 @@
-def call(String kubeconfigCredId, String manifestsPath = "k8s") {
+"""def call(String kubeconfigCredId, String manifestsPath = "k8s") {
     echo "Deploying to Kubernetes..."
     kubeconfig(credentialsId: kubeconfigCredId, serverUrl: "") {
         sh """
-        kubectl apply -f ${manifestsPath}/deployment.yaml
-        kubectl apply -f ${manifestsPath}/service.yaml
+
         """
     }
 }
+"""
 
+def call(String kubeconfigFileCredId, String manifestsPath = "k8s") {
+  echo "Deploying to Kubernetes..."
+
+  withCredentials([file(credentialsId: kubeconfigFileCredId, variable: 'KUBECONFIG')]) {
+    sh """
+      set -eux
+      kubectl version
+      kubectl apply -f ${manifestsPath}/deployment.yaml
+      kubectl apply -f ${manifestsPath}/service.yaml
+      kubectl rollout status deployment/flask-deployment
+    """
+  }
+}
